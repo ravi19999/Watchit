e929d57b821718b752528627cdad7857b6e02d94
ravi19999 <dahalravindra@gmail.com>
2020-07-13 00:13:55 +0545

    watchit -h command now prints out the documentation :pencil: :construction:

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
+const program = require('caporal');
+
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action((args) => {
+        console.log(args);
+    });
+
+program.parse(process.argv);
 
 const start = debounce(() => {
     console.log("Starting Users Program");
