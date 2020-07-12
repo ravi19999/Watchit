e85d1f4710ff5cc877dd86fd08a8f511e66a9421
ravi19999 <dahalravindra@gmail.com>
2020-07-12 23:46:26 +0545

    Debounced the add event listener :heavy_plus_sign: :sparkles:

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
 
+const start = debounce(() => {
+    console.log("Starting Users Program");
+}, 100);
+
 chokidar
-  .watch(".")
-  .on("add", () => console.log("FILE ADDED"))
-  .on("change", () => console.log("FILE CHANGED"))
-  .on("unlink", () => console.log("FILE UNLINKED"));
+    .watch(".")
+    .on("add", () => start)
+    .on("change", () => console.log("FILE CHANGED"))
+    .on("unlink", () => console.log("FILE UNLINKED"));
